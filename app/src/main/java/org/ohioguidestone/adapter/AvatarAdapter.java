814bd3bb2e8321ced0abf365096d01db9fc751c6
Krithik Rao <krdevmail@gmail.com>
2018-10-20 08:12:30 -0400

    Added Avatar Selection

--- app/src/main/java/org/ohioguidestone/adapter/AvatarAdapter.java
+++ app/src/main/java/org/ohioguidestone/adapter/AvatarAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 
 import org.ohioguidestone.R;
@@ -16,10 +17,13 @@ import java.util.List;
 
 public class AvatarAdapter extends RecyclerView.Adapter<AvatarAdapter.AvatarItemViewHolder> {
 
+    private RecyclerViewClickListener mListener;
+
     private List<Avatar> avatarList;
 
-    public AvatarAdapter(List<Avatar> avatars) {
+    public AvatarAdapter(List<Avatar> avatars, RecyclerViewClickListener listener) {
         avatarList = avatars;
+        mListener = listener;
     }
 
     @Override
@@ -28,7 +32,8 @@ public class AvatarAdapter extends RecyclerView.Adapter<AvatarAdapter.AvatarItem
         View itemView = LayoutInflater
                 .from(viewGroup.getContext())
                 .inflate(R.layout.avatar_item, viewGroup, false);
-        return new AvatarItemViewHolder(itemView);
+
+        return new AvatarItemViewHolder(itemView, mListener);
     }
 
     @Override
@@ -42,13 +47,28 @@ public class AvatarAdapter extends RecyclerView.Adapter<AvatarAdapter.AvatarItem
         return avatarList.size();
     }
 
-    public final static class AvatarItemViewHolder extends RecyclerView.ViewHolder {
+    public interface RecyclerViewClickListener {
+
+        void onClick(View view, int position);
+    }
+
+    public final static class AvatarItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private RecyclerViewClickListener mListener;
+
         ImageView avatarImage;
 
-        public AvatarItemViewHolder(View itemView) {
+        public AvatarItemViewHolder(View itemView, RecyclerViewClickListener listener) {
             super(itemView);
             avatarImage = itemView.findViewById(R.id.avatar_item_view);
+            mListener = listener;
+            itemView.setOnClickListener(this);
         }
+        @Override
+        public void onClick(View view) {
+            mListener.onClick(view, getAdapterPosition());
+        }
+
+
     }
 
 }
