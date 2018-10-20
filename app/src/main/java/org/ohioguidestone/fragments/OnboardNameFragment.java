9d85c3f82dfd45cc7a70ed7785295ff7e63374ec
Kevin <kklions1@gmail.com>
2018-10-20 01:35:29 -0400

    added stuff, i dunno

--- app/src/main/java/org/ohioguidestone/fragments/OnboardNameFragment.java
+++ app/src/main/java/org/ohioguidestone/fragments/OnboardNameFragment.java
@@ -2,15 +2,21 @@ package org.ohioguidestone.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.ohioguidestone.R;
 import org.ohioguidestone.fragments.OnboardAvatarFragment;
+import org.ohioguidestone.models.UserModel;
 
 public class OnboardNameFragment extends Fragment {
     private View fragmentView;
+    private UserModel newUser;
 
     public static OnboardNameFragment newInstance() {
         return new OnboardNameFragment();
@@ -21,4 +27,41 @@ public class OnboardNameFragment extends Fragment {
         fragmentView = inflater.inflate(R.layout.onboard_name, container, false);
         return fragmentView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Button continueButton = fragmentView.findViewById(R.id.onboarding_name_continue_btn);
+        EditText nameField = fragmentView.findViewById(R.id.user_name);
+
+        nameField.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    continueButton.setEnabled(true);
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+        continueButton.setOnClickListener((view) -> {
+            newUser = new UserModel();
+            newUser.setName(nameField.getText().toString());
+            navigateToAvatarFragment();
+        });
+        
+    }
+
+
+    public void navigateToAvatarFragment() {
+
+    }
+
 }
