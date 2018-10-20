c36d02184f0a68c4181481f00f900cebd06fe04d
Kevin <kklions1@gmail.com>
2018-10-19 21:54:45 -0400

    start working on activities contract

--- app/src/main/java/org/ohioguidestone/database/ActivitiesContract.java
+++ app/src/main/java/org/ohioguidestone/database/ActivitiesContract.java
@@ -2,14 +2,18 @@ package org.ohioguidestone.database;
 
 import android.provider.BaseColumns;
 
-public final class UserContract {
-    private UserContract() {
+public final class ActivitiesContract {
+    private ActivitiesContract() {
         // private because we don't wan't other classes to be able to create instances of this class
     }
 
-    public static class UserEntry implements BaseColumns {
+    public static class ActivitiesEntry implements BaseColumns {
         // database contract goes here, see
         // https://developer.android.com/training/data-storage/sqlite
+        public static final String TABLE_NAME = "activity";
+        public static final String COLUMN_ACTIVITY_NAME = "activityName";
+
+
 
     }
 }
