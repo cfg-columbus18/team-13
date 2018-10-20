13be00d90162cf108aa3163a4c97f49e6d0fcb07
Kevin <kklions1@gmail.com>
2018-10-20 07:31:22 -0400

    insanity

---
+++ app/src/main/java/org/ohioguidestone/activities/FeelingsActivity.java
@@ -0,0 +1,66 @@
+package org.ohioguidestone.activities;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.ohioguidestone.R;
+import org.ohioguidestone.adapter.FeelingsAdapter;
+import org.ohioguidestone.application.MindfulApplication;
+import org.ohioguidestone.fragments.OnboardNameFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeelingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feelings);
+
+        if (((MindfulApplication) this.getApplication()).getFirstStartup()) {
+            FragmentManager manager = this.getFragmentManager();
+
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.onboarding_fragment_holder, OnboardNameFragment.newInstance());
+            transaction.commit();
+        } else {
+            enableMainLayout();
+        }
+    }
+
+    public void enableMainLayout() {
+        TextView statusCheckText = findViewById(R.id.how_are_you_text);
+        statusCheckText.setVisibility(View.VISIBLE);
+
+        List<String> feelings = new ArrayList<>();
+
+        feelings.add("Happy");
+        feelings.add("Energetic");
+        feelings.add("Hopeful");
+        feelings.add("Relaxed");
+        feelings.add("Nauseous");
+        feelings.add("Tired");
+        feelings.add("Hopeless");
+        feelings.add("Brave");
+        feelings.add("Calm");
+        feelings.add("Anxious");
+        feelings.add("Worried");
+        feelings.add("Lonely");
+        feelings.add("Confused");
+        feelings.add("Irritable");
+
+        RecyclerView feelingsRecyclerView = findViewById(R.id.feelings_list_view);
+        FeelingsAdapter feelingsAdapter = new FeelingsAdapter(feelings);
+
+        feelingsRecyclerView.setAdapter(feelingsAdapter);
+        feelingsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
--- app/src/main/java/org/ohioguidestone/fragments/OnboardAvatarFragment.java
+++ app/src/main/java/org/ohioguidestone/fragments/OnboardAvatarFragment.java
@@ -12,10 +12,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import org.ohioguidestone.R;
-import org.ohioguidestone.activities.MainActivity;
+import org.ohioguidestone.activities.FeelingsActivity;
 import org.ohioguidestone.adapter.AvatarAdapter;
 import org.ohioguidestone.models.Avatar;
-import org.ohioguidestone.models.BundleKeys;
 import org.ohioguidestone.models.UserModel;
 
 import java.util.ArrayList;
@@ -79,7 +78,7 @@ public class OnboardAvatarFragment extends Fragment {
 
             transaction.remove(this);
             transaction.commit();
-            ((MainActivity) getActivity()).enableMainLayout();
+            ((FeelingsActivity) getActivity()).enableMainLayout();
 
             //TODO save user data in sharedpreferences
         });
