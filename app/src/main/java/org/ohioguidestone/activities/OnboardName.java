ce9c8b4a91ac8aa79464b77699de08a9db852fd7
takoda1 <31773273+takoda1@users.noreply.github.com>
2018-10-19 21:36:59 -0400

    Added onboard activities

---
+++ app/src/main/java/org/ohioguidestone/activities/OnboardName.java
@@ -0,0 +1,16 @@
+package org.ohioguidestone.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import org.ohioguidestone.R;
+
+public class OnboardName extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard_name);
+    }
+
+}
