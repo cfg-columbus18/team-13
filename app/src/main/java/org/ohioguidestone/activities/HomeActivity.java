2c29e82ed0dd7941fa2d40d2e5b36eb95121d664
takoda1 <31773273+takoda1@users.noreply.github.com>
2018-10-20 07:28:00 -0400

    Intents from home page to other pages

--- app/src/main/java/org/ohioguidestone/activities/HomeActivity.java
+++ app/src/main/java/org/ohioguidestone/activities/HomeActivity.java
@@ -1,10 +1,12 @@
 package org.ohioguidestone.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.ohioguidestone.R;
@@ -17,9 +19,14 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //get preference for activity from intent passed by daily preference page
+        //string/activity_id preference =
+
         findViewById(R.id.socialCard).setOnClickListener(toggleVisibility(R.id.socialActivityDescription));
         findViewById(R.id.relaxCard).setOnClickListener(toggleVisibility(R.id.relaxActivityDescription));
         findViewById(R.id.physicalCard).setOnClickListener(toggleVisibility(R.id.physicalActivityDescription));
+
 
         SharedPreferences sharedPref = this.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
@@ -29,6 +36,61 @@ public class HomeActivity extends Activity {
         String name = sharedPref.getString(getString(R.string.saved_name_key), "Boom Shaka");
         ((TextView) findViewById(R.id.userName)).setText(name);
 
+        //choose activities from database based on preferences
+        //socialAcitivityId = MostPopularOption based on tag selected from daily preference screen
+
+
+        TextView socialActivity = (TextView) findViewById(R.id.socialActivityDescription);
+        socialActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(), SettingsActivity.class);
+                intent.putExtra("activity", "ID FOR ACTIVITY");
+                startActivity(intent);
+            }
+        });
+
+        TextView relaxActivity = (TextView) findViewById(R.id.relaxActivityDescription);
+        relaxActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(), SettingsActivity.class);
+                intent.putExtra("activity", "ID FOR ACTIVITY");
+                startActivity(intent);
+            }
+        });
+        TextView physicalActivity = (TextView) findViewById(R.id.physicalActivityDescription);
+        physicalActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(), SettingsActivity.class);
+                intent.putExtra("activity", "ID FOR ACTIVITY");
+                startActivity(intent);
+            }
+        });
+
+        Button settings = (Button) findViewById(R.id.settingsButton);
+        Button summary = (Button) findViewById(R.id.summaryButton);
+
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //Create the intent to start another activity
+                Intent intent = new Intent(view.getContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //Create the intent to start another activity
+                Intent intent = new Intent(view.getContext(), SummaryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private View.OnClickListener toggleVisibility(int view) {
