65f9665b83578ed6b6791325c3217110e37197fa
Kevin <kklions1@gmail.com>
2018-10-19 20:08:14 -0400

    add user table contract stub

---
+++ app/src/main/java/org/ohioguidestone/database/UserContract.java
@@ -0,0 +1,14 @@
+package org.ohioguidestone.database;
+
+import android.provider.BaseColumns;
+
+public final class UserContract {
+    private UserContract() {
+        // private because we don't wan't other classes to be able to create instances of this class
+    }
+
+    public static class UserEntry implements BaseColumns {
+        // database contract goes here, see
+        // https://developer.android.com/training/data-storage/sqlite
+    }
+}
